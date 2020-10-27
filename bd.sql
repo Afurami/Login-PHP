235b438f053961d4ea2fce120669170ad0de38cb
Afurami <73539165+Afurami@users.noreply.github.com>
2020-10-27 08:45:53 -0600

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,7 @@
+/* Crear base de datos; */
+create database usuario;
+	/* crear tabla de la base de datos */
+	create table login(
+	  `email` varchar (100) not null,
+	  `password` int (50) not null
+		) ;
---
+++ index.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+<link href="style.css" rel="stylesheet" id="bootstrap-css">
+</head>
+<body>
+
+<div class="container">
+    	<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="panel panel-login">
+					<div class="panel-heading">
+						<div class="row">
+							<div class="col-xs-6">
+								<a href="#" class="active" id="login-form-link">Iniciar sesión</a>
+							</div>
+							<div class="col-xs-6">
+								<a href="#" id="register-form-link">Registrar</a>
+							</div>
+						</div>
+						<hr>
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-lg-12">
+								<form id="login-form" action="login.php" method="post" role="form" style="display: block;">
+									<div class="form-group">
+										<input type="text" name="email" id="email" tabindex="1" class="form-control" placeholder="Correo" value="" required>
+									</div>
+									<div class="form-group">
+										<input type="password" name="pass" id="pass" tabindex="2" class="form-control" placeholder="Contraseña" required>
+									</div>
+									<div class="form-group text-center">
+										<input type="checkbox" tabindex="3" class="" name="remember" id="remember">
+										<label for="remember">Recordar</label>
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-sm-6 col-sm-offset-3">
+												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Ingresar">
+											</div>
+										</div>
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-lg-12">
+												<div class="text-center">
+													<a href="" tabindex="5" class="forgot-password">Recuperar contraseña?</a>
+												</div>
+											</div>
+										</div>
+									</div>
+								</form>
+								<form id="register-form" action="registrar.php" method="post" role="form" style="display: none;">
+									<div class="form-group">
+										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Correo" value="" required>
+									</div>
+									<div class="form-group">
+										<input type="password" name="pass" id="pass" tabindex="2" class="form-control" placeholder="Contraseña" required>
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-sm-6 col-sm-offset-3">
+												<input type="submit" name="grabardatos" id="grabardatos" tabindex="4" class="form-control btn btn-register" value="Registrar">
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</body>
+	</html>
